fb05429a99409b4d94cad30c2efaae084dc89844
Andrea Donadello <andreadonadello@tutanota.com>
2023-12-12 12:31:08 +0100

    Scripts

--- bin/start-all.sh
+++ bin/start-all.sh
@@ -19,6 +19,7 @@ sbin/start-dfs.sh
 sleep 5
 
 #start spark
+export SPARK_IDENT_STRING=spark1
 cd $FWK_PATH/spark-2.4.0-bin-hadoop2.7_1 || exit
 sbin/start-master.sh --port 8001 --webui-port 8011
 sleep 2
@@ -26,6 +27,7 @@ sbin/start-slave.sh tesi.homenet.telecomitalia.it:8001
 
 sleep 5
 
+export SPARK_IDENT_STRING=spark2
 cd $FWK_PATH/spark-2.4.0-bin-hadoop2.7_2 || exit
 sleep 2
 sbin/start-master.sh --port 8002 --webui-port 8012
@@ -34,6 +36,7 @@ sbin/start-slave.sh tesi.homenet.telecomitalia.it:8002
 
 sleep 5
 
+export SPARK_IDENT_STRING=spark3
 cd $FWK_PATH/spark-2.4.0-bin-hadoop2.7_3 || exit
 sleep 2
 sbin/start-master.sh --port 8003 --webui-port 8013
--- bin/stop-all.sh
+++ bin/stop-all.sh
@@ -4,17 +4,20 @@
 FWK_PATH="/home/andrea/bin"
 
 cd $FWK_PATH/spark-2.4.0-bin-hadoop2.7 || exit
+export SPARK_IDENT_STRING=spark1
 sbin/stop-slave.sh tesi.homenet.telecomitalia.it:8001
 sbin/stop-master.sh --port 8001 --webui-port 8011
 
 sleep 2
 
+export SPARK_IDENT_STRING=spark2
 sbin/stop-slave.sh tesi.homenet.telecomitalia.it:8002
 sbin/stop-master.sh --port 8002 --webui-port 8012
 
 sleep 2
 
-op-slave.sh tesi.homenet.telecomitalia.it:8003
+export SPARK_IDENT_STRING=spark3
+sbin/stop-slave.sh tesi.homenet.telecomitalia.it:8003
 sbin/stop-master.sh --port 8003 --webui-port 8013
 
 cd $FWK_PATH/hadoop-2.9.2 || exit
