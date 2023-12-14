2c25353ecbae04ec682e72f1021be9ecc18a7c9e
Andrea Donadello <andreadonadello@tutanota.com>
2023-12-14 17:08:22 +0100

    Trying

--- src/main/scala/it/utiu/thesis/machinelearing/DiabetesTrainerActor.scala
+++ src/main/scala/it/utiu/thesis/machinelearing/DiabetesTrainerActor.scala
@@ -36,7 +36,7 @@ class DiabetesTrainerActor extends AbstractClassificationTrainerActor {
     val df2 = assembler.transform(df1)
 
     val splitSeed = new Random().nextInt()
-    val Array(trainingData, testData) = df2.randomSplit(Array(0.6, 0.4), splitSeed)
+    val Array(trainingData, testData) = df2.randomSplit(Array(0.9, 0.1), splitSeed)
     val trainCount = trainingData.count()
     val testCount = testData.count()
     println("Training count:" + trainCount)
