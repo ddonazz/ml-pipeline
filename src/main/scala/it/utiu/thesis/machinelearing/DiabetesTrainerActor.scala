f1025660819df6fb09d5f3f020c760feaa1293b7
Andrea Donadello <andreadonadello@tutanota.com>
2023-12-14 16:24:58 +0100

    Trying

--- src/main/scala/it/utiu/thesis/machinelearing/DiabetesTrainerActor.scala
+++ src/main/scala/it/utiu/thesis/machinelearing/DiabetesTrainerActor.scala
@@ -52,7 +52,7 @@ class DiabetesTrainerActor extends AbstractClassificationTrainerActor {
     weightedData.show(false)
 
     //LOGISTIC REGRESSION CLASSIFIER
-    val lr = new LogisticRegression().setRegParam(0.01).setElasticNetParam(0.3)
+    val lr = new LogisticRegression().setRegParam(0.01).setElasticNetParam(0.001)
       .setLabelCol("label")
       .setFeaturesCol("features")
       .setFamily("multinomial")
